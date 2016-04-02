23313e08abcbf903fbbdaf0c9da9e2b828ddbf97
P.Y. Laligand <py.laligand@gmail.com>
2016-04-02 11:14:00 -0700

    Made the library work again
    Main changes:
    - added spec file
    - updated build file, matching https://github.com/google/dart-sync-
    socket/blob/master/tool/build.sh
    - updated method names in native extension
    - updated syntax in the Dart library
    - adapted the tests to the dart-lang/test runner
    
    Note: because of https://github.com/dart-lang/sdk/issues/26113, this
    won't properly work until the next release of Dart. However, this change
    was tested with a local build of the VM including the fix and all tests
    passed.

---
+++ test/sqlite_test.dart
@@ -0,0 +1,123 @@
+import 'dart:io';
+
+import 'package:path/path.dart' as path;
+import 'package:test/test.dart';
+
+import '../lib/sqlite.dart' as sqlite;
+
+createBlogTable(db) {
+  db.execute("CREATE TABLE posts (title text, body text)");
+}
+
+typedef DatabaseTest(sqlite.Database db);
+
+runWithConnectionOnDisk(DatabaseTest dbTest) {
+  final fileName = path.join(
+      Directory.systemTemp.createTempSync("dart-sqlite-test-").path,
+      'db.sqlite');
+  final db = new sqlite.Database(fileName);
+  try {
+    dbTest(db);
+  } finally {
+    db.close();
+  }
+}
+
+runWithConnectionInMemory(DatabaseTest dbTest) {
+  final db = new sqlite.Database.inMemory();
+  try {
+    dbTest(db);
+  } finally {
+    db.close();
+  }
+}
+
+testRunner(DatabaseTest dbTest) {
+  return () {
+    runWithConnectionOnDisk(dbTest);
+    runWithConnectionInMemory(dbTest);
+  };
+}
+
+void main() {
+  test('first', testRunner((db) {
+    var row = db.first("SELECT ?+2, UPPER(?)", params: [3, "hello"]);
+    expect(row[0], equals(5));
+    expect(row[1], equals("HELLO"));
+  }));
+
+  test('row', testRunner((db) {
+    var row = db.first("SELECT 42 AS foo");
+    expect(row.index, equals(0));
+
+    expect(row[0], equals(42));
+    expect(row['foo'], equals(42));
+    expect(row.foo, equals(42));
+
+    expect(row.asList(), equals(const [42]));
+    expect(row.asMap(), equals(const {"foo": 42}));
+  }));
+
+  test('bulk', testRunner((db) {
+    createBlogTable(db);
+    var insert = db.prepare("INSERT INTO posts (title, body) VALUES (?,?)");
+    try {
+      expect(insert.execute(params: ["hi", "hello world"]), equals(1));
+      expect(insert.execute(params: ["bye", "goodbye cruel world"]), equals(1));
+    } finally {
+      insert.close();
+    }
+    var rows = [];
+    expect(
+        db.execute("SELECT * FROM posts", callback: (row) {
+          rows.add(row);
+        }),
+        equals(2));
+    expect(rows.length, equals(2));
+    expect(rows[0].title, equals("hi"));
+    expect(rows[1].title, equals("bye"));
+    expect(rows[0].index, equals(0));
+    expect(rows[1].index, equals(1));
+    rows = [];
+    expect(
+        db.execute("SELECT * FROM posts", callback: (row) {
+          rows.add(row);
+          return true;
+        }),
+        equals(1));
+    expect(rows.length, equals(1));
+    expect(rows[0].title, equals("hi"));
+  }));
+
+  test('transaction success', testRunner((db) {
+    createBlogTable(db);
+    expect(db.transaction(() {
+      db.execute("INSERT INTO posts (title, body) VALUES (?,?)");
+      return 42;
+    }), equals(42));
+    expect(db.execute("SELECT * FROM posts"), equals(1));
+  }));
+
+  test('transaction failure', testRunner((db) {
+    createBlogTable(db);
+    expect(db.execute("SELECT * FROM posts"), equals(0));
+    try {
+      db.transaction(() {
+        db.execute("INSERT INTO posts (title, body) VALUES (?,?)");
+        throw new UnsupportedError("whee");
+      });
+      fail("Exception should have been propagated");
+    } on UnsupportedError catch (_) {}
+    expect(db.execute("SELECT * FROM posts"), equals(0));
+  }));
+
+  test('syntax error', testRunner((db) {
+    expect(() => db.execute("random non sql"),
+        new Throws(new isInstanceOf<sqlite.SqliteSyntaxException>()));
+  }));
+
+  test('column error', testRunner((db) {
+    expect(() => db.first("select 2+2")['qwerty'],
+        new Throws(new isInstanceOf<sqlite.SqliteException>()));
+  }));
+}
