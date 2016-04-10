79144192cc864120a429955f37ec4cb7e1bb458a
P.Y. Laligand <py.laligand@gmail.com>
2016-04-09 21:50:11 -0700

    Added analysis options. (#27)
    Fixed a bunch of lints as well.
    
    Fixes #16

--- test/sqlite_test.dart
+++ test/sqlite_test.dart
@@ -5,13 +5,13 @@ import 'package:test/test.dart';
 
 import '../lib/sqlite.dart' as sqlite;
 
-createBlogTable(db) {
+_createBlogTable(db) {
   db.execute("CREATE TABLE posts (title text, body text)");
 }
 
-typedef DatabaseTest(sqlite.Database db);
+typedef void _DatabaseTest(sqlite.Database db);
 
-runWithConnectionOnDisk(DatabaseTest dbTest) {
+_runWithConnectionOnDisk(_DatabaseTest dbTest) {
   final fileName = path.join(
       Directory.systemTemp.createTempSync("dart-sqlite-test-").path,
       'db.sqlite');
@@ -23,7 +23,7 @@ runWithConnectionOnDisk(DatabaseTest dbTest) {
   }
 }
 
-runWithConnectionInMemory(DatabaseTest dbTest) {
+_runWithConnectionInMemory(_DatabaseTest dbTest) {
   final db = new sqlite.Database.inMemory();
   try {
     dbTest(db);
@@ -32,21 +32,21 @@ runWithConnectionInMemory(DatabaseTest dbTest) {
   }
 }
 
-testRunner(DatabaseTest dbTest) {
+_testRunner(_DatabaseTest dbTest) {
   return () {
-    runWithConnectionOnDisk(dbTest);
-    runWithConnectionInMemory(dbTest);
+    _runWithConnectionOnDisk(dbTest);
+    _runWithConnectionInMemory(dbTest);
   };
 }
 
 void main() {
-  test('first', testRunner((db) {
+  test('first', _testRunner((db) {
     final row = db.first("SELECT ?+2, UPPER(?)", params: [3, "hello"]);
     expect(row[0], equals(5));
     expect(row[1], equals("HELLO"));
   }));
 
-  test('row', testRunner((db) {
+  test('row', _testRunner((db) {
     final row = db.first("SELECT 42 AS foo");
     expect(row.index, equals(0));
 
@@ -57,8 +57,8 @@ void main() {
     expect(row.asMap(), equals(const {"foo": 42}));
   }));
 
-  test('bulk', testRunner((db) {
-    createBlogTable(db);
+  test('bulk', _testRunner((db) {
+    _createBlogTable(db);
     var insert = db.prepare("INSERT INTO posts (title, body) VALUES (?,?)");
     try {
       expect(insert.execute(params: ["hi", "hello world"]), equals(1));
@@ -88,8 +88,8 @@ void main() {
     expect(rows[0]['title'], equals("hi"));
   }));
 
-  test('transaction success', testRunner((db) {
-    createBlogTable(db);
+  test('transaction success', _testRunner((db) {
+    _createBlogTable(db);
     expect(db.transaction(() {
       db.execute("INSERT INTO posts (title, body) VALUES (?,?)");
       return 42;
@@ -97,8 +97,8 @@ void main() {
     expect(db.execute("SELECT * FROM posts"), equals(1));
   }));
 
-  test('transaction failure', testRunner((db) {
-    createBlogTable(db);
+  test('transaction failure', _testRunner((db) {
+    _createBlogTable(db);
     expect(db.execute("SELECT * FROM posts"), equals(0));
     try {
       db.transaction(() {
@@ -110,12 +110,12 @@ void main() {
     expect(db.execute("SELECT * FROM posts"), equals(0));
   }));
 
-  test('syntax error', testRunner((db) {
+  test('syntax error', _testRunner((db) {
     expect(() => db.execute("random non sql"),
         new Throws(new isInstanceOf<sqlite.SqliteSyntaxException>()));
   }));
 
-  test('column error', testRunner((db) {
+  test('column error', _testRunner((db) {
     expect(() => db.first("select 2+2")['qwerty'],
         new Throws(new isInstanceOf<sqlite.SqliteException>()));
   }));
