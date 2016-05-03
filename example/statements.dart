3bf07df94a0edb3bfa0869dcb471079ae4ebf7b0
Brett Morgan <brett.morgan@gmail.com>
2016-05-04 08:53:57 +1000

    Update example
    Fix for https://github.com/pylaligand/dart-sqlite/issues/55

--- example/statements.dart
+++ example/statements.dart
@@ -3,47 +3,49 @@
 // You may obtain a copy of the License at
 // http://www.apache.org/licenses/LICENSE-2.0
 
-import 'dart:async';
-
 import 'package:sqlite/sqlite.dart';
 
-_runSimpleQuery(Database db) async {
-  Row row =
-      await db.query('SELECT ?+16, UPPER(?)', params: [2000, 'Ligue 1']).first;
+_runSimpleQuery(Database db) {
+  Row row = db.first('SELECT ?+16, UPPER(?)', params: [2000, 'Ligue 1']);
   final int year = row[0];
   final String league = row[1];
   print('-- $league $year --');
 }
 
-_createAndInsertEntries(Database db) async {
-  await db.execute('CREATE TABLE rankings (team text, points int)');
-  await db.execute('INSERT INTO rankings VALUES ("Nice", 38)');
-  await db.execute('INSERT INTO rankings VALUES ("Monaco", 41)');
+_createAndInsertEntries(Database db) {
+  db.execute('CREATE TABLE rankings (team text, points int)');
+  db.execute('INSERT INTO rankings VALUES ("Nice", 38)');
+  db.execute('INSERT INTO rankings VALUES ("Monaco", 41)');
   final count =
-      (await db.query("SELECT COUNT(*) AS count FROM rankings").first).count;
+      (db.first("SELECT COUNT(*) AS count FROM rankings")).asMap()['count'];
   print('$count teams competing');
 }
 
 _useStatements(Database db) async {
-  await db.execute('INSERT INTO rankings VALUES ("PSG", 78)');
-  await db.execute('INSERT INTO rankings VALUES ("Lyon", 39)');
-  final teams = db.query("SELECT * FROM rankings");
-  final count = await teams.length;
+  db.execute('INSERT INTO rankings VALUES ("PSG", 78)');
+  db.execute('INSERT INTO rankings VALUES ("Lyon", 39)');
+
+  num count = 0;
+  db.execute("SELECT * FROM rankings", callback: (Row row) {
+    count++;
+  });
   print('$count teams competing now');
 }
 
 _inspectResults(Database db) async {
-  final subscription = db
-      .query('SELECT * FROM rankings ORDER BY points DESC')
-      .listen((Row row) => print('${row.team.padRight(10)} ${row.points}'));
-  await subscription.asFuture();
+  db.execute('SELECT * FROM rankings ORDER BY points DESC',
+      callback: (Row row) {
+    var r = row.asMap();
+    print('${r["team"].padRight(10)} ${r["points"]}');
+    return true;
+  });
   print('Who\'s the best now?');
 }
 
-Future main(List<String> args) async {
+main(List<String> args) {
   final db = new Database.inMemory();
-  await _runSimpleQuery(db);
-  await _createAndInsertEntries(db);
-  await _useStatements(db);
-  await _inspectResults(db);
+  _runSimpleQuery(db);
+  _createAndInsertEntries(db);
+  _useStatements(db);
+  _inspectResults(db);
 }
